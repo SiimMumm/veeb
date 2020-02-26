e4346432046b18cec6263bde520d3445d783e879
Siim <siim.mumm@khk.ee>
2020-02-26 09:02:55 +0200

    Loodud eelkonfiguratsioon

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,4 +1,26 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-login
+<!-- login form -->
+<div class="row">
+  <div class="col-md-6 mx-auto">
+    <div class="card card-body bg-light mt-5">
+      <h2>Create an Account</h2>
+      <form action="<?php echo URLROOT;?>/users/register" method="post">
+        <div class="form-group">
+          <label for="email">Email: <sup>*</sup></label>
+          <input type="text" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="pass">Password: <sup>*</sup></label>
+          <input type="password" id="pass" class="form-control">
+        </div>
+        <div class="row">
+          <div class="col">
+            <input type="submit" value="Login" class="btn btn-block bg-info">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
 
